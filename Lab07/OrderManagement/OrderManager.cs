b0323e8fd76102cc40dd7947f3d4bb0a19b4e0c7
tiiedye <tiiedye@gmail.com>
2020-11-16 11:56:19 -0800

    adding project files

---
+++ Lab07/OrderManagement/OrderManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement
+{
+    public class OrderManager
+    {
+        public double CalculateShipping(double weightKG)
+        {
+            double cost = 0;
+            if (weightKG <= 0)
+            {
+                throw new ArgumentException("Weight must be positive");
+            }
+            else if (weightKG < 1)
+            {
+                cost = 2.5;
+            }
+            else if (weightKG < 5)
+    }
+}
